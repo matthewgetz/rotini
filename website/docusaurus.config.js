e509b9346e2dce87427a2252b9d0c92aed78a116
Matt Getz <matthewcgetz@gmail.com>
2023-02-06 21:06:03 -0700

    rotini v1.0.1

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ module.exports = {
   tagline: 'a framework for building node.js cli programs',
   titleDelimiter: '·',
   url: 'https://matthewgetz.github.io/',
-  baseUrl: '/rotini/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
